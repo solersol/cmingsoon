a44adc61c225cdc4c82ba07fdf40ad67e3c8790f
solersol <ttpage1tk@gmail.com>
2024-08-09 19:07:43 +0300

    Vdf

--- src/sections/FloatingImages.jsx
+++ src/sections/FloatingImages.jsx
@@ -5,14 +5,14 @@ const FloatingImages = () => {
   return (
     <section className="w-full h-screen flex justify-center items-center">
       <div className="flex space-x-8 mt-32">
-        <a href="https://x.com/SolSnoozy" target="_blank" rel="noopener noreferrer">
+        <a href="https://x.com/SnuggySol" target="_blank" rel="noopener noreferrer">
           <img
             src={img1}
             alt="Link to Site 1"
             className="w-[100px] h-auto md:w-[150px] lg:w-[200px] float-animation transition-transform duration-300 transform hover:scale-105"
           />
         </a>
-        <a href="https://t.me/SNOOZY2MILLIONS" target="_blank" rel="noopener noreferrer">
+        <a href="https://t.me/SNUGGYTOMILLIONZ" target="_blank" rel="noopener noreferrer">
           <img
             src={cardImage1}
             alt="Link to Site 2"
